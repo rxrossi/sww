0231341951919e10a7ca7605c96b32cb5bfeb140
Alex <rx.rossi@gmail.com>
2022-10-13 21:28:50 +0100

    Merge: Add esbuild command
    commit de0b480741cd3ccc49faa7f8b4b9f517c19137b7
    Author: Alex <rx.rossi@gmail.com>
    Date:   Thu Oct 13 21:28:18 2022 +0100
    
        Add esbuild command

---
+++ packages/core/build.js
@@ -0,0 +1,26 @@
+const { build } = require("esbuild");
+const { dependencies = {}, peerDependencies = {} } = require("./package.json");
+
+const shared = {
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+};
+
+build({
+  ...shared,
+  outfile: "dist/index.js",
+});
+
+build({
+  ...shared,
+  outfile: "dist/index.esm.js",
+  format: "esm",
+});
+
+const { Generator } = require("npm-dts");
+
+new Generator({
+  entry: "src/index.ts",
+  output: "dist/index.d.ts",
+}).generate();
---
+++ packages/core/src/index.ts
@@ -0,0 +1,1 @@
+export { buildSdk } from "src/sdk/application";
